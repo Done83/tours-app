31b174ae5e3bc8bc59199a004d5a5eb5d2fd1ec4
Done83 <nedomehan@gmail.com>
2024-02-10 12:36:32 +0100

    Tours component completed

--- src/components/ToursComponent.jsx
+++ src/components/ToursComponent.jsx
@@ -1,6 +1,20 @@
-const ToursComponent = () => {
-  return (
-    <h2>ToursComponent</h2>
-  )
-}
-export default ToursComponent
+import TourComponent from './TourComponent';
+
+/* eslint-disable react/prop-types */
+const ToursComponent = ({ tours }) => {
+    console.log(tours);
+    return (
+        <section className='flex flex-col w-[90%] max-w-[90%] items-center justify-center mt-8 mx-auto'>
+            <div className='container mx-auto flex flex-col justify-center items-center'>
+                <h2 className='text-3xl'>Our Tours</h2>
+                <span className=' bg-teal-500 h-[5px] w-[100px] rounded mt-2'></span>
+            </div>
+            <div className='mt-10 flex gap-3'>
+                {tours.map((tour) => {
+                    return <TourComponent key={tour.id} />;
+                })}
+            </div>
+        </section>
+    );
+};
+export default ToursComponent;
